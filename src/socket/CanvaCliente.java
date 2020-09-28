1629f7388612740d5a3be6c2e221760975b241e9
Joel <joel.araya97@users.noreply.github.com>
2020-09-27 20:18:25 -0600

    Se implementó un servidor con un puerto que lo identifique con el usuario, es posible mantener conversaciones

--- src/socket/CanvaCliente.java
+++ src/socket/CanvaCliente.java
@@ -3,25 +3,27 @@ package socket;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class CanvaCliente extends JPanel {
+public class CanvaCliente extends JPanel implements Runnable {
 
     private JTextArea areaTextoCliente;
-    private JTextField campoEscribir, ip;
+    private JTextField campoEscribir, Contacto;
     private JButton boton;
+    int puertoInicial = 50000;
 
     public CanvaCliente() {
 
         JLabel texto = new JLabel("Chat --- Introduce tu Ip");   //Muestra un Label que dice que este es la ventana del chat
         add(texto);
 
-        ip = new JTextField(7);
-        add(ip);
+        Contacto = new JTextField(7);
+        add(Contacto);
 
         areaTextoCliente = new JTextArea(24, 35);
         add(areaTextoCliente);
@@ -35,24 +37,62 @@ public class CanvaCliente extends JPanel {
         boton.addActionListener(evento);
 
         add(boton);
+
+        Thread t2 = new Thread(this);
+        t2.start();
+    }
+
+    @Override
+    public void run(){
+        boolean flag = true;
+        while (flag) {
+            try {
+                ServerSocket servidor = new ServerSocket(puertoInicial);
+                String puertoEmisor, mensaje;
+                MensajeUsuario mensajeRecibido;
+                flag = false;
+                System.out.println("===============");
+                System.out.println(puertoInicial);
+                System.out.println("===============");
+                while (true) {
+                    Socket socket = servidor.accept();
+                    ObjectInputStream datos = new ObjectInputStream(socket.getInputStream());
+                    mensajeRecibido = (MensajeUsuario) datos.readObject();
+
+                    puertoEmisor = mensajeRecibido.getIp();
+                    mensaje = mensajeRecibido.getMensaje();
+
+                    areaTextoCliente.append("De " + puertoEmisor + ": " + mensaje + "\n");
+
+                    socket.close();
+                }
+            } catch (UnknownHostException unknownHostException) {
+                unknownHostException.printStackTrace();
+                System.out.println("UnknownHostException cliente");
+            } catch (IOException ioException) {
+                //ioException.printStackTrace();
+                System.out.println("IOException cliente");
+                puertoInicial++;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private class TextoEnviado implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
 
-            int puerto = 50000;
+            int puerto = Integer.parseInt(Contacto.getText());
 
             try {
-                System.out.println(ip.getText() + ":  " + campoEscribir.getText());
-
-                Socket nuevoSocket = new Socket("127.0.0.1", 50000);
-
+                Socket nuevoSocket = new Socket("127.0.0.1", puerto);
 
                 MensajeUsuario datosRecibidos = new MensajeUsuario();
-                datosRecibidos.setIp(ip.getText());
+                datosRecibidos.setIp(String.valueOf(puertoInicial));
                 datosRecibidos.setMensaje(campoEscribir.getText());
 
+                areaTextoCliente.append("Yo: " + campoEscribir.getText() + "\n");
 
                 ObjectOutputStream informacionDatos = new ObjectOutputStream(nuevoSocket.getOutputStream());
                 informacionDatos.writeObject(datosRecibidos);
--- src/socket/MensajeUsuario.java
+++ src/socket/MensajeUsuario.java
@@ -6,6 +6,14 @@ public class MensajeUsuario implements Serializable {
 
     private String ip, mensaje;
 
+    public MensajeUsuario() {
+    }
+
+    public MensajeUsuario(String ip, String mensaje) {
+        this.ip = ip;
+        this.mensaje = mensaje;
+    }
+
     public String getIp() {
         return ip;
     }
